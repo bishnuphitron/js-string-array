cd7a5decf78a9533464c9d5498cdf5dbf90533be
bsnun8 <bishnunath018@gmail.com>
2022-02-24 16:30:35 +0600

    Apply Search includes, indexOf, startswith, endswith

---
+++ string-search.js
@@ -0,0 +1,41 @@
+const products = [
+  'Dell hardcore i29 200GB laptop',
+  'iphone 1TB camera flashlight Phone',
+  'yellow laptop with black camera',
+  'Dell 1X59 Lenovo commercial yoga laptop',
+  'LG supernova laptop Dell',
+  'HTC low price Phone',
+  'Dell purple color phone with Laptop'
+];
+
+const searching = 'DeLL';
+
+// indexOf
+const output = [];
+for (const product of products) {
+  if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+    // output.push(product);
+  }
+}
+
+// includes()
+for (const product of products) {
+  if (product.toLowerCase().includes(searching.toLowerCase())) {
+    // output.push(product);
+  }
+}
+
+// starts with
+for (const product of products) {
+  if (product.toLowerCase().startsWith(searching.toLowerCase())) {
+    // output.push(product);
+  }
+}
+
+// ends with
+for (const product of products) {
+  if (product.toLowerCase().endsWith(searching.toLowerCase())) {
+    output.push(product);
+  }
+}
+console.log(output);
