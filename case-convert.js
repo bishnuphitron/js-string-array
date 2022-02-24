98fd83ca2b1d6053ce8375c7c84eee76998a9144
bsnun8 <bishnunath018@gmail.com>
2022-02-24 12:45:14 +0600

    String comparison using toLowerCase toUpperCase

---
+++ case-convert.js
@@ -0,0 +1,7 @@
+const userInput = 'blACkPinK';
+const savedUserName = 'blackPink';
+// console.log(userInput.toLowerCase());
+// console.log(savedUserName.toLowerCase());
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+  console.log('user exists');
+}
